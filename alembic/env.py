be09344f80fd28a2248cede9769dfbe7ca90bc42
m.chernobrov <m.chernobrov@aerodisk.ru>
2025-04-22 20:36:08 +0300

    (fix): Замена стартовой миграции

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,11 @@
 import asyncio
 from logging.config import fileConfig
 
-from alembic import context
-from sqlalchemy import Connection, pool
-from sqlalchemy.ext.asyncio import (
-    async_engine_from_config,
-)
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from bot.database.registry import metadata
+from alembic import context # type: ignore[attr-defined]
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,17 +20,18 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+from bot.database.schema import mapper_registry
+target_metadata = mapper_registry.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
 config.set_main_option(
     'sqlalchemy.url',
     'postgresql+asyncpg://bj_user:bj_pass@localhost/blackjack'
 )
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
