94908505d18c31053886b10f2180340040806a8f
Mikhail Chernobrov <m.chernobrov@aerodisk.ru>
2025-08-25 17:57:29 +0300

    (feat): Change read config from .env

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,10 @@
 from aiohttp import web
 
 from bot.api.routes import setup_routes
-from bot.settings import config
+from bot.settings import app_config
 
 if __name__ == "__main__":
     app = web.Application()
     setup_routes(app)
-    app["config"] = config
+    app["config"] = app_config
     web.run_app(app)
--- bot/settings.py
+++ bot/settings.py
@@ -1,13 +1,49 @@
-from pathlib import Path
-from typing import Any
+import os
+from dataclasses import dataclass
 
-import yaml
+from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-config_path = BASE_DIR / 'config.yaml'
 
-def load_config(path: Path) -> Any:
-    with open(path) as f:
-        return yaml.safe_load(f)
+@dataclass(frozen=True)
+class DatabaseConfig:
+    driver_name: str | None
+    username: str | None
+    password: str | None
+    host: str | None
+    port: str | None
+    database: str | None
 
-config = load_config(config_path)
+    def get_connection_url(self) -> str:
+        return (
+            f"{self.driver_name}://"
+            f"{self.username}:{self.password}@"
+            f"{self.host}:{self.port}/"
+            f"{self.database}"
+        )
+
+
+@dataclass(frozen=True)
+class BotConfig:
+    token: str | None
+
+
+@dataclass(frozen=True)
+class Config:
+    database: DatabaseConfig
+    bot: BotConfig
+
+
+def get_config(env_name: str) -> Config:
+    load_dotenv(env_name)
+    bot_config = BotConfig(token=os.getenv("BOT_TOKEN"))
+    database_config = DatabaseConfig(
+        driver_name=os.getenv("POSTGRES_DRIVER"),
+        username=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+        database=os.getenv("POSTGRES_DB"),
+    )
+    return Config(database=database_config, bot=bot_config)
+
+app_config = get_config(".env")
